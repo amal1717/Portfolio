23fa9c069eb45518cc82808a37fadc952fdca273
amal1717 <amal1717@student.eadania.dk>
2020-11-30 10:21:42 +0100

    Ny php - header
    Det er lavet en php-fil til headeren i includes-mappen

--- Includes/header.php
+++ Includes/header.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="da" dir="ltr"> 
-    
-    <head> 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name= "description" content="En læringsportfolio som er udviklet af en multimediedesignerstuderende"> <!--En beskrivelse af hjemmesiden-->
-        
-        <base href="index.html">
-        
-        <title>Amalies portfolio</title> <!--Navngivelse af fanen i browseren-->
-        
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        
-        <link rel="stylesheet" href="index.css"> <!--Indsæt CSS stylesheet efter href="-->
-        
-        <link rel="stylesheet" href="https://use.typekit.net/tyy2njs.css">
-        <link rel="stylesheet" href="https://use.typekit.net/tyy2njs.css">
-         
-    </head>
-    
-    <body>
-        
         <header class="header">
                 
             <nav class="navbar navbar-expand-lg navbar-light bg-custom">
@@ -100,58 +77,4 @@
                 
             </nav>
             
-        </header>    
-            
-                <section>
-                    
-                  
-                 
-                    <div id="mitgrid">
-
-                      
-                        <div id="Billedeogsearch">
-                            
-                            <img src="Billeder/Baggrundsbillede%20af%20CSS.jpg" alt="CSS" style="width:100%;">
-                            
-                            <div id="search"> 
-                            <form class="form-inline my-2 my-lg-0">
-                              <input id="searchbox" class="form-control mr-sm-2" type="search" placeholder="Søg" aria-label="Search">
-                            </form> 
-                            </div>
-                            
-                        </div>
-                            
-                        
-                        <section id="felt2">
-                              
-                        </section>
-
-                        <section id="felt3">
-                            <p></p>
-                        </section>
-
-                        <section id="felt4">
-                            <p></p>
-                        </section>
-
-                        <section id="felt5">
-                            <p></p>
-                        </section>
-
-                    </div>
-
-                </section>
-            
-            <footer>
-                <p>&copy; Amalie Sørensen 2020</p>
-            </footer>        
-        
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        
-    </body>
-
-</html>
-
-
+        </header>   
