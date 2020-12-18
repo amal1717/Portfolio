b5ff64eeec9efecee9eaa4b8cc3e10634abb7caf
amal1717 <amal1717@student.eadania.dk>
2020-12-18 18:05:35 +0100

    Oprettelse af ophavsretoglisencering.php

---
+++ ophavsretoglicensering.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+
+<html lang="da" dir="ltr"> 
+    
+    <head> 
+        <meta charset="utf-8"> <!--Det danske alfabet-->
+        <meta name="viewport" content="width=device-width, initial-scale=1"/> <!--For at sikre mobile view-->
+        <meta name= "description" content="En læringsportfolio som er udviklet af en multimediedesignerstuderende"> <!--En beskrivelse af hjemmesiden-->
+        
+        <title>Amalies læringsportfolio</title> <!--Navngivelse af fanen i browseren-->
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> <!--Stylesheet fra Bootstrap-->
+        
+        <link rel="stylesheet" href="html.css"> <!--Indsæt CSS stylesheet efter href="-->
+        <link rel="stylesheet" href="global.css"> <!--Indsæt CSS stylesheet efter href="-->
+        
+        <link rel="stylesheet" href="https://use.typekit.net/tyy2njs.css"> <!--Stylesheet fra Adobe Fonts-->
+         
+    </head>
+    
+    <body>
+        
+        <header class="header">
+                
+       <?php include('includes/header.php'); ?> <!--Inkluderer menuen-->
+            
+        </header>  
+        
+           
+                <section id="mitgrid">
+                    
+                        <section id="oversigtoversiden">
+                            <nav>
+                                <ul>
+                                    <li><h2 id="oversigt">Oversigt:</h2></li>
+                                    <li><a class="list-item" href="#ophavsret">Ophavsret</a></li>
+                                    <li><a class="list-item" href="#socialemedier">Ophavsret på de sociale medier</a></li>
+                                    <li><a class="list-item" href="#licensaftaler">Licensaftaler</a></li>
+                                    <li><a class="list-item" href="#refleksion">Refleksion</a></li>
+                                    <li><a class="list-item" href="#kilder">Kilder</a></li>
+                                </ul>
+                            </nav>
+                        </section>
+                  
+                        <section id="">
+                            <h2>Ophavsret</h2>
+                            <p>Ophavsret, også kaldet copyright, markeres med mærket © og anvendes som rettighed i forbindelse med kunstneriske eller litterære værker, som f.eks. billeder, film, bøger osv., men også i forbindelse med edb-programmer. For at kunne opnå ophavsret skal ”værket” have en vis værkshøjde, som betyder at værket skal være frembragt af personen selv og have en vis form for originalitet. Når et værk opnår ophavsret, betyder det bl.a. at kunstneren har ret til betaling, når andre gør brug af værket og ret til selv at disponere værket (Systime , 2020) 
+                                <br><br>
+                            Når der er tale om et ophavsbeskyttet værk er det tilladt at citere fra værket og tilladt at kopirer værket til privat forbrug. Dog må værket ikke anvendes eller ændres på, så det er krænkende for ophavsmanden. Derudover har ophavsmanden krav på at blive nævnt, når værket anvendes.
+                            Som multimediedesigner skal man derfor være opmærksom på f.eks. at redigere i fotografier uden ophavsret f.eks. via Pexels.com, Unsplash.com eller Colourbox.dk. Medmindre man gerne vil nævne ophavsmanden og være påpasselig med ikke at krænke ophavsmanden (LegalDesk, 2020). 
+                                <br><br>
+                            Som multimediedesigner støder man ofte på copyright, bl.a. i forbindelse med hjemmesider, logoer, edb-programmer og lignende, så derfor skal man huske at være opmærksom på dette og undersøge det først inden man lige ”låner” noget, da der hurtigt kan opstå̊ en ophavsretskrænkelse. 
+                            </p>
+                            
+                            <h3>Ophavsret på de sociale medier</h3>
+                            <p>Der gælder de samme regler angående ophavsretten på de sociale medier som alle andre steder på nettet. Man må ikke dele musik, billeder, videoer eller lignende uden tilladelse fra rettighedshaveren, men man skal også være opmærksom på at mange af de sociale medier kræver en licens til at må bruge alt det materiale, som man lægger ud på din profil. 
+                                <br><br>
+                            Der er ingen specifikke regler i ophavsretsloven angående linking, men i 2014 kom EU-domstolene frem til at det ikke er tilladt at linke materiale på internettet, hvis linket omgår restriktive foranstaltninger og på den måde giver en adgang, der ellers ikke var mulig (Kultur Ministeriet, 2020). 
+                            </p>
+                            
+                        </section>
+                    
+                        <section>
+                            <h2>Licensaftaler</h2>
+                            <p>En licensaftale er en aftale mellem to parter, hvor licensgiver (indehaver af rettighed) giver licenstager (virksomhed eller privat) tilladelse til at bruge rettigheden mod licensafgift (betaling). Vilkårene i licensaftalen fastlægger hvordan licenstager må råde over rettigheden (Patent- og varemærkestyrelsen, 2020).</p>
+                            
+                            <h3>jQuery</h3>
+                            <p>jQuery fra JavaScript er udgivet under MIT-licensen, som er en simpel licens, hvor den eneste betingelse er at copyright og licensmeddelelsen skal bevares i materialet. Ellers må den ændres og distribueres samt anvendes til både kommerciel og privat brug. Dog inkluderer licensen en begrænsning af ansvaret og giver ikke nogen garanti (MIT License, 2020).</p>
+                                <br><br>
+                            <img class="billeder" src="Billeder/Ophavsret/mit_license.png" alt="Billede af MIT License">
+                            <p>(MIT License, 2020)</p>
+                            
+                            <h3>Bootstrap</h3>
+                            <p>Bootstrap er også udgivet under MIT-licensen og har copyright 2018 Twitter. Betingelsen for at anvende Bootstrap er blot at man skal er at opbevare licens- og copyrightmeddelelsen inkluderet i Bootstraps CSS- og JavaScript-filer, når du bruger dem i dine værker (License FAQs, 2020).</p>
+                            
+                            <h3>Creative Commons</h3>
+                            <p>Creative Commons er en organisation, som tilbyder til række åbne og gratis licenser, som alle kan anvende, hvis man vil offentliggøre et værk. Licenserne gør det muligt for ophavsmanden at give andre tilladelse til at bruge værket, dog under nogle bestemte betingelser. Licenserne giver nemlig ophavsmaden mulighed for at vælge hvordan andre må bruge værket. Derfor skal ophavsmanden vælge den licenseringsform som vedkommende synes virker mest tiltrækkende (Hvad er Creative Commons?, 2020).</p>
+                            
+                                <br>
+                            
+                            <p><a href="pdf_filer/Ophavsret_og_licensering.pdf">Se opgaven om ophavsret og licensering</a></p>
+                            
+                        </section>
+                    
+                    
+                    <section>
+                        <h2 id="refleksion">Refleksion</h2>
+                        <p></p>
+                    </section>
+                    
+                    
+                    <section>
+                        <h2 id="kilder">Kilder</h2>
+                        <h3>Internetkilder:</h3>
+                        <p>
+                            Systime . (3. november 2020). Immaterielle rettigheder - Teori . Hentet fra i.systime.dk: <a href="https://i.systime.dk/?id=432">https://i.systime.dk/?id=432</a> 
+                                <br><br>
+                            Patent- og varemærkestyrelsen. (3. november 2020). Licensaftaler. Hentet fra Patent- og varemærkestyrelsen: <a href="http://www.ip-handelsportal.dk/aftaleindgaaelse/valg-af-aftale/licensaftaler.aspx">http://www.ip-handelsportal.dk/aftaleindgaaelse/valg-af-aftale/licensaftaler.aspx</a> 
+                                <br><br>
+                            MIT License. (3. november 2020). Hentet fra choosealicense.com: <a href="https://choosealicense.com/licenses/mit/">https://choosealicense.com/licenses/mit/</a> 
+                                <br><br>
+                            License FAQs. (3. november 2020). Hentet fra getbootstrap.com: <a href="https://getbootstrap.com/docs/4.0/about/license/">https://getbootstrap.com/docs/4.0/about/license/</a>
+                                <br><br>
+                            LegalDesk. (4. november 2020). Ophavsret. Hentet fra legaldesk.dk: <a href="https://www.legaldesk.dk/artikler/ophavsret">https://www.legaldesk.dk/artikler/ophavsret</a>
+                                <br><br>
+                            Kultur Ministeriet. (6. oktober 2020). Ophavsret på internettet. Hentet fra kum.dk: <a href="https://kum.dk/kulturomraader/ophavsret/ophavsret-paa-internettet">https://kum.dk/kulturomraader/ophavsret/ophavsret-paa-internettet</a>
+                                <br><br>
+                            Hvad er Creative Commons? (3. november 2020). Hentet fra medierforalle.skoleblogs.dk: <a href="https://medierforalle.skoleblogs.dk/hvad-er-creative-commons">https://medierforalle.skoleblogs.dk/hvad-er-creative-commons</a>
+                        </p>
+                        
+                        <br><br>
+                        
+                    </section>
+
+                </section> <!--MITGRID SLUTTER HER-->
+        
+            <footer>
+                <p>&copy; Amalie Sørensen 2020</p>
+            </footer>        
+        
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        
+    </body>
+
+</html>
