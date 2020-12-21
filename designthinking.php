83707b4a6e091e4c9ea1ae4465a93c5af1e41c1e
amal1717 <amal1717@student.eadania.dk>
2020-12-21 13:41:35 +0100

    Ændringer i designthinking.php og hackathon.php

--- designthinking.php
+++ designthinking.php
@@ -27,46 +27,101 @@
         </header>  
         
                 <section id="mitgrid">
+                    
+                       <section id="oversigtoversiden">
+                            <nav> 
+                                <ul>
+                                    <li><h2 id="oversigt">Oversigt:</h2>
+                                    <li><a class="list-item" href="#projektet">Design Thinking projektet</a></li>
+                                    <li><a class="list-item" href="#videoafleveringer">Videoafleveringer</a></li>
+                                      <li><a class="list-item" href="#designthinking">Design Thinking metoden</a></li>
+                                    <li><a class="list-item" href="#refleksion">Refleksion</a></li>
+                                    <li><a class="list-item" href="#kilder">Kilder</a></li>
+                                </ul>
+                            </nav> 
+                        </section>
                 
-                  
-                    <section id="">
-                        <h2>Hackathon</h2>
-                        <p></p>
+                    
+                    <section id="projektet">
                         
-                            <br><br>
-                   
-                        <img src="Billeder/Hackathon/Design-Thinking-Model.png" alt="Design thinking model" style="width:100%">
-                      
+                            <h2>Design Thinking projektet</h2>
+                        
+                            <p>Dette projekt fokuserede på design thinking metoden og derfor var projektet opdelt i faser, hvor hver fase skulle gennemføres før næste fase måtte påbegyndes. Design thinking metoden består af fem faser, så derfor var projektet også opdelt i fem faser. Dog havde projektet også en sjette fase, implementering, som i dette tilfælde handlede om at "sælge" vores idé. Emnet for projektet var FN’s 17 verdensmål hvor vi ved hjælp af design thinking skulle finde en relevant problemstilling og derefter udvikle en idé til en app, som kunne bidrage til at løse denne problemstilling. I løbet af projektet skulle vi aflevere nogle forskellige videoer hvor vi bl.a. fortalte om hvordan vi havde grebet processen an, hvad vi fandt ud af og hvilket resultat vi var kommet frem til.</p>
+                        
+                                <br><br>
+
+                            <img class="billeder" src="Billeder/Design_Thinking_app/design-thinking-app1.png" alt="Design thinking" style="width:100%">
+                        
+                                <br><br><br>
+
+                            <img class="billeder" src="Billeder/Design_Thinking_app/design-thinking-app2.png" alt="Design thinking" style="width:100%">
+                    
+                    </section>
+                    
+                    <section id="videoafleveringer">
+                            
+                            <h2>Emphathize og define</h2>
+                            <div class="video-container">
+                                <iframe class="youtubevideo" width="560" height="315" 
+                                src="https://www.youtube.com/embed/47VGWfYEfB4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>  
+
+                            <h2>Resultat af emphathize og define</h2>
+                            <div class="video-container"> 
+                                <iframe class="youtubevideo" width="560" height="315" src="https://www.youtube.com/embed/MfjJ2JVyFgo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>    
+
+                            <h2>Ideation, prototyping og test</h2>
+                            <div class="video-container">
+                                <iframe class="youtubevideo" width="560" height="315"  src="https://www.youtube.com/embed/p8POz3wIDpU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+
+                            <h2>Pitch</h2>
+                            <div class="video-container">
+                                <iframe class="youtubevideo" width="560" height="315" src="https://www.youtube.com/embed/nDt5zsYqVls" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+
+                            <h2>Resultatet</h2>
+                            <div class="video-container">
+                                <iframe class="youtubevideo" width="560" height="315"  src="https://www.youtube.com/embed/yhRMCSsBOzE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                    </section>
+                
+                                      
+                    <section id="designthinking">
+                        
+                            <h2>Design Thinking</h2>
+
+                            <p>Design thinking er en innovationsmetode som bidrager til at identificere de faktiske udfordringer samt brugernes reelle behov og ikke hvad man tror, de har behov for. Metoden hjælper nemlig med at identificere de rigtige spørgsmål, så man ikke går i gang med at udvikle et produkt der ikke løser den faktiske udfordring (Ditmer, 2020). Design thinking metoden består af fem faser, emphathize, define, ideate, prototype og test, som skal følges i samme rækkefølge for at der opnås et optimalt resultat.</p>
+
+
+                            <h3>1.	Empathize</h3>
+                            <p>Denne fase handler om at indsamle information, være nysgerrige og forstå konteksten. Det er vigtigt, at man bruger en del tid i denne fase, da resultatet af denne fase danner baggrund for hele forløbet. Man kan hurtigt komme til at antage hvad udfordringen er, men hvis det ikke er undersøgt tilstrækkeligt, er der blot tale om en antagelse (Hackathon - Fase 1, 2020). Man skal være opmærksom på at løsningsforslag ikke hører til i denne fase, så derfor skal disse blot nedskrives og gemmes til ideate-fasen (Design Thinking projekt - Fase 1 , 2020).</p>
+
+
+                            <h3>2.	Define</h3>
+                            <p>Denne fase handler om at skabe mening ud af alt den viden man fik i empathize-fasen og definerer den helt rigtige problemstilling, som projektet skal tage udgangspunkt i. Det er vigtigt den indsamlede data er visuelt, håndgribeligt og tilgængeligt præsenteret, f.eks. ved at benytte whiteboards eller lignende. (Hackathon - Fase 2 , 2020)</p>
+
+
+                            <h3>3.	Ideate</h3>
+                            <p>Denne fase går ud på at udvikle så mange idéer som muligt til potentielle løsninger. Det er vigtigt, at man fortsætter med at idéudvikle selvom man får en favoritidé, fordi man skal skubbe til forestillingen om hvad der kan lade sig gøre og give plads til mindre oplagte idéer (Hackathon - fase 3, 2020). I denne fase skal man altså ikke være kritisk, men være åben og fordomsfri (Design Thinking projekt - fase 3, 2020).</p>
+
+
+                           <h3>4.	Prototype</h3>
+                            <p>Denne fase handler om at udvikle en fysisk repræsentation af den idé man valgte i ideate-fasen. I starten arbejdes med lavpraktiske og billige prototyper, idet man først skal bruge flere ressourcer på prototyper når man nærmer sig en mere færdig løsning. Prototyping er et godt redskab til at gøre idéen mere håndgribelig og få andre til at forstå idéen bedre (Hackathon - fase 4, 2020).</p>
+
+
+                            <h3>5.	Test</h3>
+                            <p>Denne fase handler om at teste prototypen, helst mange gange, så man opnår troværdigt feedback, som man kan bruge til at forbedre løsningen. Det er vigtigt, at man ikke forsøger at påvirke testpersonerne og samtidig har et åbent sind i forhold til den feedback som de giver (Hackathon - fase 5, 2020).</p>
+
+
+                                <br><br>
+
+                            <img src="Billeder/Hackathon/Design-Thinking-Model.png" alt="Design thinking model" style="width:100%">
+                        
+                            <p>(Hackathon, 2020)</p>
                         
                     </section>
-                            
-                    <h2>Emphathize og define</h2>
-                    <div class="video-container">
-                        <iframe class="youtubevideo" width="560" height="315" 
-                        src="https://www.youtube.com/embed/47VGWfYEfB4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>  
-                    
-                    <h2>Resultat af emphathize og define</h2>
-                    <div class="video-container"> 
-                        <iframe class="youtubevideo" width="560" height="315" src="https://www.youtube.com/embed/MfjJ2JVyFgo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>    
-                        
-                    <h2>Ideation, prototyping og test</h2>
-                    <div class="video-container">
-                        <iframe class="youtubevideo" width="560" height="315"  src="https://www.youtube.com/embed/p8POz3wIDpU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                        
-                    <h2>Pitch</h2>
-                    <div class="video-container">
-                        <iframe class="youtubevideo" width="560" height="315" src="https://www.youtube.com/embed/nDt5zsYqVls" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                        
-                    <h2>Resultatet</h2>
-                    <div class="video-container">
-                        <iframe class="youtubevideo" width="560" height="315"  src="https://www.youtube.com/embed/yhRMCSsBOzE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                    
-                
                    
                     <section>
                         <h2 id="refleksion">Refleksion</h2>
@@ -78,18 +133,45 @@
                         <h3>Internetkilder:</h3>
                         <p>
                             
+                            Design Thinking projekt - Fase 1 . (5. oktober 2020). Hentet fra MMD 2020-2021 Design Thinking projek: <a href="https://eadania.mrooms.net/course/view.php?id=2759#section-6">https://eadania.mrooms.net/course/view.php?id=2759#section-6</a>
+                            
+                                <br><br>
+                            
+                            Design Thinking projekt - fase 3. (7. oktober 2020). Hentet fra MMD 2020-2021 Design Thinking projekt: <a href="https://eadania.mrooms.net/course/view.php?id=2759#section-8
+                            ">https://eadania.mrooms.net/course/view.php?id=2759#section-8
+                            </a>
+                            
+                                <br><br>
+                        
+                            Ditmer. (20. december 2020). Hvad er Design Thinking, og hvorfor skal du arbejde med det? Hentet fra ditmer.dk: <a href="https://www.ditmer.dk/viden/hvad-er-design-thinking-og-hvorfor-skal-du-arbejde-med-det/">https://www.ditmer.dk/viden/hvad-er-design-thinking-og-hvorfor-skal-du-arbejde-med-det/</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - Fase 1. (2. september 2020). Hentet fra Design thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2370">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2370</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - Fase 2 . (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2371">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2371</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 3. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2372">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2372</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 4. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2373">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2373</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 5. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2374">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2374</a>
+                            
+                                <br><br>
                             
                             Hackathon. (2. september 2020). Hentet fra Sommerstart 2020: <a href="https://eadania.mrooms.net/course/view.php?id=2216#section-10">https://eadania.mrooms.net/course/view.php?id=2216#section-10</a>
-                  
-                             <br><br>
                             
-                            
-                            skivefesival.dk. (2. september 2020). Hentet fra skivefestival.dk: <a href="https://skivefestival.dk">https://skivefestival.dk</a>
-                            
+                                <br><br>
                             
                         </p>
-                         
-                            <br><br> 
                        
                     </section>
 
--- hackathon.php
+++ hackathon.php
@@ -27,25 +27,69 @@
         </header>  
         
                 <section id="mitgrid">
-                
-                  
-                    <section id="">
+                    
+                       <section id="oversigtoversiden">
+                            <nav> 
+                                <ul>
+                                    <li><h2 id="oversigt">Oversigt:</h2>
+                                    <li><a class="list-item" href="#hackathon">Hackathon projektet</a></li>
+                                    <li><a class="list-item" href="#billeder">Billeder af prototypen</a></li>
+                                     <li><a class="list-item" href="#designthinking">Design Thinking metoden</a></li>
+                                    <li><a class="list-item" href="#refleksion">Refleksion</a></li>
+                                    <li><a class="list-item" href="#kilder">Kilder</a></li>
+                                </ul>
+                            </nav> 
+                        </section>
+                    
+                    
+                    <section id="hackathon">
                         <h2>Hackathon</h2>
-                        <p></p>
-                        
-                            <br><br>
-                        <img src="Billeder/Hackathon/Design-Thinking-Model.png" alt="Design thinking model">
-                             <br><br>
-                        <img class="billeder" src="Billeder/Hackathon/hackathon1.png" alt="">
-                            <br><br>
-                        <img class="billeder" src="Billeder/Hackathon/hackathon2.png" alt="">
-                            <br><br>
-                        <img class="billeder" src="Billeder/Hackathon/hackathon3.png" alt="">
-                            <br><br>
-                        <img class="billeder" src="Billeder/Hackathon/hackathon4.png" alt="">
-                        
-                            
+                        <p>Dette projekt handlede om at anvende design thinking til at udvikle løsninger, som skulle afhjælpe Skive Festival med deres forskellige udfordringer. Skive Festival var ude på skolen for at give en præsentation om selve festivallen og deres udfordringer, hvorefter vi skulle arbejde i grupper og forbedre et oplæg, som vi skulle præsentere for hele skolen og Skive Festival til sidst i projektet. I grupperne skulle vi ved hjælp af de 5 faser i design thinking metoden vælge en af Skive Festivals udfordringer og finde frem til en konkret problemstilling, som vi skulle finde på løsningsforslag til. Derefter skulle vi vælge hvilke idéer vi ville tage udgangspunkt i og lave en prototype som vi skulle teste inden oplægget. </p>
                     </section>
+                    
+                        <br><br>
+                        
+                    <section id="billeder">
+                        
+                        <img class="billeder" src="Billeder/Hackathon/Hackathon1.png" alt="Prototype af design thinking projekt - Skive festival">
+                            <br><br>
+                        <img class="billeder" src="Billeder/Hackathon/Hackathon2.png" alt="Prototype af design thinking projekt - Skive festival">
+                        
+                    </section>
+                    
+                    <section id="designthinking">
+                        <h2>Design Thinking</h2>
+                        
+                         <p>Design thinking er en innovationsmetode som bidrager til at identificere de faktiske udfordringer samt brugernes reelle behov og ikke hvad man tror, de har behov for. Metoden hjælper nemlig med at identificere de rigtige spørgsmål, så man ikke går i gang med at udvikle et produkt der ikke løser den faktiske udfordring (Ditmer, 2020). Design thinking metoden består af fem faser, emphathize, define, ideate, prototype og test, som skal følges i samme rækkefølge for at der opnås et optimalt resultat.</p>
+
+
+                        <h3>1.	Empathize</h3>
+                        <p>Denne fase handler om at indsamle information, være nysgerrige og forstå konteksten. Det er vigtigt, at man bruger en del tid i denne fase, da resultatet af denne fase danner baggrund for hele forløbet. Man kan hurtigt komme til at antage hvad udfordringen er, men hvis det ikke er undersøgt tilstrækkeligt, er der blot tale om en antagelse (Hackathon - Fase 1, 2020). Man skal være opmærksom på at løsningsforslag ikke hører til i denne fase, så derfor skal disse blot nedskrives og gemmes til ideate-fasen (Design Thinking projekt - Fase 1 , 2020).</p>
+
+
+                        <h3>2.	Define</h3>
+                        <p>Denne fase handler om at skabe mening ud af alt den viden man fik i empathize-fasen og definerer den helt rigtige problemstilling, som projektet skal tage udgangspunkt i. Det er vigtigt den indsamlede data er visuelt, håndgribeligt og tilgængeligt præsenteret, f.eks. ved at benytte whiteboards eller lignende. (Hackathon - Fase 2 , 2020)</p>
+
+
+                        <h3>3.	Ideate</h3>
+                        <p>Denne fase går ud på at udvikle så mange idéer som muligt til potentielle løsninger. Det er vigtigt, at man fortsætter med at idéudvikle selvom man får en favoritidé, fordi man skal skubbe til forestillingen om hvad der kan lade sig gøre og give plads til mindre oplagte idéer (Hackathon - fase 3, 2020). I denne fase skal man altså ikke være kritisk, men være åben og fordomsfri (Design Thinking projekt - fase 3, 2020).</p>
+
+
+                        <h3>4.	Prototype</h3>
+                        <p>Denne fase handler om at udvikle en fysisk repræsentation af den idé man valgte i ideate-fasen. I starten arbejdes med lavpraktiske og billige prototyper, idet man først skal bruge flere ressourcer på prototyper når man nærmer sig en mere færdig løsning. Prototyping er et godt redskab til at gøre idéen mere håndgribelig og få andre til at forstå idéen bedre (Hackathon - fase 4, 2020).</p>
+
+
+                        <h3>5.	Test</h3>
+                        <p>Denne fase handler om at teste prototypen, helst mange gange, så man opnår troværdigt feedback, som man kan bruge til at forbedre løsningen. Det er vigtigt, at man ikke forsøger at påvirke testpersonerne og samtidig har et åbent sind i forhold til den feedback som de giver (Hackathon - fase 5, 2020).</p>
+                        
+                            <br><br>
+                        
+                        <img src="Billeder/Hackathon/Design-Thinking-Model.png" alt="Design thinking model">
+                        
+                        <p>(Hackathon, 2020)</p>
+                    
+                    </section>
+                    
                 
                    
                     <section>
@@ -57,15 +101,45 @@
                         <h2 id="kilder">Kilder</h2>
                         <h3>Internetkilder:</h3>
                         <p>
+                            Design Thinking projekt - Fase 1 . (5. oktober 2020). Hentet fra MMD 2020-2021 Design Thinking projek: <a href="https://eadania.mrooms.net/course/view.php?id=2759#section-6">https://eadania.mrooms.net/course/view.php?id=2759#section-6</a>
                             
+                                <br><br>
+                            
+                            Design Thinking projekt - fase 3. (7. oktober 2020). Hentet fra MMD 2020-2021 Design Thinking projekt: <a href="https://eadania.mrooms.net/course/view.php?id=2759#section-8
+                            ">https://eadania.mrooms.net/course/view.php?id=2759#section-8
+                            </a>
+                            
+                                <br><br>
+                        
+                            Ditmer. (20. december 2020). Hvad er Design Thinking, og hvorfor skal du arbejde med det? Hentet fra ditmer.dk: <a href="https://www.ditmer.dk/viden/hvad-er-design-thinking-og-hvorfor-skal-du-arbejde-med-det/">https://www.ditmer.dk/viden/hvad-er-design-thinking-og-hvorfor-skal-du-arbejde-med-det/</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - Fase 1. (2. september 2020). Hentet fra Design thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2370">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2370</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - Fase 2 . (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2371">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2371</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 3. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2372">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2372</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 4. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2373">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2373</a>
+                            
+                                <br><br>
+                            
+                            Hackathon - fase 5. (3. september 2020). Hentet fra Design Thinking - faseforståelse og redskaber: <a href="https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2374">https://eadania.mrooms.net/mod/book/view.php?id=138356&chapterid=2374</a>
+                            
+                                <br><br>
                             
                             Hackathon. (2. september 2020). Hentet fra Sommerstart 2020: <a href="https://eadania.mrooms.net/course/view.php?id=2216#section-10">https://eadania.mrooms.net/course/view.php?id=2216#section-10</a>
-                  
-                             <br><br>
                             
+                                <br><br>
                             
                             skivefesival.dk. (2. september 2020). Hentet fra skivefestival.dk: <a href="https://skivefestival.dk">https://skivefestival.dk</a>
-                            
                             
                         </p>
                          
